6ae4dd253ff5b706a3336c68542fa4917861e6ca
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-17 11:08:54 -0500

    added to the return statement for Login.js

--- src/component/login.js
+++ src/component/login.js
@@ -8,11 +8,33 @@ class Login extends React.Component {
             password: '',
          };
     }
-    render() {
+    render(){
         return (
+          <div className="reactapp">
             
+           <h1 className="title" >REACT BASE SIGN UP FORM</h1>
+      
+            {/* used onchange to call the function that will catch the event that updates the state when text is entered in the input boxes */}
+            <form onChange={this.onInput} >         
+              <div>
+                <label>Username</label>
+                <input type='text' name="username" />
+              </div>
+      
+              <div>
+                <label>Password</label>
+                {/* 3. type passwrod hides the word you enter */}
+                <input type='password' name="password" />
+              </div>
+            </form>
+      
+          {/* added onClick calls this.onInput */}
+          {/* the page doesn't auto refresh so I don't pass an event */}
+          <button className='btn' onClick={() => this.onSubmit()}> {this.onSubmit} Login</button>
+      
+         </div>
         );
+      }
     }
-}
 
 export default Login; 
