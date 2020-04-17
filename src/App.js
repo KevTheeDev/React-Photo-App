49fc5a717cc9e18ef472f8f3864b86aa6100a1c1
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-17 09:37:49 -0500

    made comp folder, inserted hmtl into app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+
+     };
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+            <li><a href="#" className="active">Home </a></li>
+
+            <li className="dropdown">
+                <a className="dropdown" href="#">About </a>
+            </li>
+
+            <li><a href="#">Next</a></li>
+        </ul>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
