71cc48bb5631938ee7e0eb28b76ec1fa6845ab20
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-17 10:50:33 -0500

    deleted line for app.css

---
+++ src/component/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            username: '',
+            password: '',
+         };
+    }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default Login;
