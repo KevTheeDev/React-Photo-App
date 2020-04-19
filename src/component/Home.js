d814cb499314761aaae245ddce07f71e94839469
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-18 20:58:38 -0500

    router, linnk, and nav eelement in the render of Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Router, Link, } from 'react-native';
 
 function Home(){
     return(
         <React.Fragment>
             <h3> Home Page </h3>
+            <Router>
+                <nav> 
+                <ul>
+                    <li>
+                    <Link to="/Home">Home</Link>
+                    </li>
+                </ul>
+                </nav >
+            </Router >
         </React.Fragment>
     )
 }
