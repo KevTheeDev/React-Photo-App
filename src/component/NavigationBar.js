b162d7a0f7be1f4a879e38e2c084a5e80621e61f
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-18 19:50:01 -0500

    added NavigationBar.js to componenr and inserted content into it

---
+++ src/component/NavigationBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class NavigationBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <ul className="navList">
+                    <li><a className="active"> Home </a></li>
+
+                    <li className="dropdown">
+                        <a className="dropdown" > User Profile </a>
+                    </li>
+
+                    <li><a> Bookmark and Saved Images </a></li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default NavigationBar;
