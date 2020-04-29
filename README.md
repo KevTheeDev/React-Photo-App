ea9698d93da3e0fe71b32d34b95eeea96ad1d51f
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-29 02:53:12 -0500

    updated README.md, created folder for image gallery

--- README.md
+++ README.md
@@ -19,3 +19,22 @@ https://i.pinimg.com/originals/82/fe/6a/82fe6ad700689bc3639509a50b3de11c.jpg
 3. Profile Pic
 https://i.ytimg.com/vi/NYcWNP9XN-k/maxresdefault.jpg
 
+4. Image Gallery
+
+a.  
+https://i.pinimg.com/originals/7e/e3/4e/7ee34e7030a3ef6b1ece2f4b784ac205.jpg
+
+b.          
+https://s1.cdn.autoevolution.com/images/news/2020-bmw-3-series-rendered-with-race-car-concept-kit-132092_1.jpg
+
+c.  
+https://ae01.alicdn.com/kf/Hed64be684083437a87d687a269c44cb00/Creative-Concept-Simple-Watches-Men-Cool-Street-Brand-Clock-for-YOUNG-2020-New-Trendy-Wristwatch-Waterproof.jpg
+
+d.      
+https://uk.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-run-away-trainer-shoes--AD9U1ASC02_PM1_Side%20view.jpg
+
+e.      
+https://s7d1.scene7.com/is/image/BHLDN/54523386_011_d?$pdpmain$
+
+f.      
+https://media.karousell.com/media/photos/products/2017/06/08/custom_diamond_cartier_sunglasses_1496896043_df582584.jpg
--- src/component/Home.js
+++ src/component/Home.js
@@ -3,16 +3,34 @@ import home_redandblue_bkcgrnd from '../images/home_redandblue_bkcgrnd.jpg'
 import './Home.css'
 import { Route, BrowserRouter as Router, Link, Switch, } from 'react-router-dom';
 
-function Home(){
+class Home extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            imageContainerArr: [
+
+            ],
+        }
+    }
+
+
+    render(){
+        let image = this.state.imageContainerArr;
+        const imagesMap = image.map;
     return(
         <div>
             <h1 className="hmpg"> Home Page  </h1>
-            <div>
-                <img src={ home_redandblue_bkcgrnd } className="home_background" alt="bluered" />
-            </div>
+                <div>
+                    <img src={ home_redandblue_bkcgrnd } className="home-background" alt="blue-red" />
+                    <div className="image-container" >
+
+                    </div>
+                </div>
         </div>
-    )
+        )
+    }
 }
+
  
 export default Home;
  
